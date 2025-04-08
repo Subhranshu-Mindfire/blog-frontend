20e70cb3ab8aed8c40a9d07a084a3f3239e36f12
subhranshu <subhranshu.mfs@gmail.com>
2025-04-09 01:09:59 +0530

    3: Fixed Router and added UserProvider to wrap the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Login from './components/Login';
+import { UserProvider } from './context/UserContext.jsx';
 
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/logout" element={<Logout />} />
-      </Routes>
+      <UserProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
+
+
     </>
   );
 };
